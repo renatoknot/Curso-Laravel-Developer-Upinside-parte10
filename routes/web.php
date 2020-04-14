10adf36244ece7a1b52ba8b7e26f1551f2f02381
Renato Gonçalves <renatoslip@hotmail.com>
2020-04-14 14:05:05 -0300

    Adequando Layout ao Laravel

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'as' => 'admin.'], function(){
+    Route::get('/', 'AuthController@showLoginForm')->name('login');
+});
